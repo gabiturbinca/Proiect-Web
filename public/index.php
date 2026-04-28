1dfdcc6438b09bb33fd9f996157f2ac09b6e4fbc
stanciuana <stanciuana62@gmail.com>
2026-04-28 18:08:51 +0300

    adaugat header si footer

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php 
+    $page_title='Giw-Acasă';
+    $page_cs= 'home';
+    require __DIR__ . '/../templates/header.php';
+?>
+
+<section>
+    <h1>Găsește cadou!!</h1>
+</section>
+
+<?php 
+    require __DIR__ . '/../templates/footer.php';
+?>
---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+const toggle = document.querySelector('.navbar__toggle');
+const menu   = document.getElementById('primary-menu');
+
+toggle?.addEventListener('click', () => {
+  const isOpen = menu.classList.toggle('is-open');
+  toggle.setAttribute('aria-expanded', String(isOpen));
+  toggle.setAttribute('aria-label', isOpen ? 'Închide meniul' : 'Deschide meniul');
+});
