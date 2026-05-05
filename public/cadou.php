84ec1e0c453eaa1756fe465c5d4152507719e0af
stanciuana <stanciuana62@gmail.com>
2026-05-05 19:24:48 +0300

    am terminat designul la cadou

--- public/cadou.php
+++ public/cadou.php
@@ -6,21 +6,26 @@
 ?>
 
     <section class="gift__container">
-        <div id="gift__header">
-            <h1>Buchet Trandafiri Rosii</h1>
-            <img src="https://cdn.pixabay.com/photo/2014/04/14/20/11/pink-324175_1280.jpg" alt="Buchet Trandafiri Rosii">
-        </div>
+        <h1 class="gift__title">Buchet Trandafiri Rosii</h1>
 
-        <div class="gift__details">
-            <p class="gift__description"><strong>Descriere: </strong>Buchet de 25 trandafiri rosii</p>
-            <p class="gift_price">Pret: 149.99</p>
-            <p class="gift__brand__name">Brand: FloraExpress</p>
-            <p class="gift__category">Categorie: Flori</p>
-        </div>
+        <div class="gift__row">
+            <div id="gift__header">
+                <img src="https://cdn.pixabay.com/photo/2014/04/14/20/11/pink-324175_1280.jpg" alt="Buchet Trandafiri Rosii">
+            </div>
 
-        <div class="gift__buttons">
-            <button class="btn btn--primary">Trimite cadou</button>
-            <button class="btn btn--ghost">Adaugă la favorite</button>
+            <div id="gift__mini__container">
+                <div class="gift__details">
+                    <p class="gift__description"><strong>Descriere: </strong>Buchet de 25 trandafiri rosii</p>
+                    <p class="gift_price">Pret: 149.99</p>
+                    <p class="gift__brand__name">Brand: FloraExpress</p>
+                    <p class="gift__category">Categorie: Flori</p>
+                </div>
+
+                <div class="gift__buttons">
+                    <button class="btn btn--primary">Trimite cadou</button>
+                    <button class="btn btn--ghost">Adaugă la favorite</button>
+                </div>
+            </div>
         </div>
     </section>
 
