4c321e08d42511bdc0599a970efc09d9daf0189e
stanciuana <stanciuana62@gmail.com>
2026-05-25 11:02:11 +0300

    am adaugat cancel order in pagina de comenzileMele

--- public/js/pages/comenzileMele.js
+++ public/js/pages/comenzileMele.js
@@ -1,6 +1,3 @@
-//get commands per page
-//pageNumber -> a cata pagina
-//elemNumber -> cate per pagina
 const perPage = 5;
 let currentPage = 1;
 
@@ -8,13 +5,23 @@ async function getCommands(page){
     const URL=`/api/orders?pageNumber=${page}&elemNumber=${perPage}`;
     const res = await apiFetch(URL);
     if(!res.ok){
-        throw new Error("Couldn't fetch the user commands");
+        throw new Error("Couldn't fetch the user orders");
     }
     const data = await res.json();
     console.log(data.success);
     return data.success;
 }
 
+async function cancelOrder(orderId){
+    const URL = `/api/orders/${orderId}/cancel`;
+    const res = await apiFetch(URL, {
+        method : "PATCH", 
+
+    });
+    const data = await res.json();
+    return { ok: res.ok, body: data };
+
+}
 function createCommandCard(command_details){
     const template = document.getElementById("commands_template");
     const card = template.content.firstElementChild.cloneNode(true);
@@ -24,10 +31,12 @@ function createCommandCard(command_details){
     card.querySelector(".card__description").textContent = " " + command_details.description;
     card.querySelector(".card__quantity").textContent = " " + command_details.quantity;
     card.querySelector(".card__status").textContent = " " + command_details.status;
+    card.dataset.orderId=command_details.id;
+
     return card;
 }
 
-async function renderCommandsPage(page){
+async function renderOrdersPage(page){
     if(page<1) return;
     const commandBox=document.getElementById("commands_cards");
     const nextBtn = document.getElementById("btn__next");
@@ -41,18 +50,39 @@ async function renderCommandsPage(page){
        commandBox.replaceChildren(...orderCards);
        prevBtn.disabled = (page === 1);
        nextBtn.disabled = (page === nrPages);
+
+       const cancelBtns = document.querySelectorAll(".btn-cancel-order");
+       cancelBtns.forEach(cancelBtn => {
+            cancelBtn.addEventListener("click", async(e) => {
+                const state = cancelBtn.parentElement.querySelector(".card__status").textContent.trim();
+                console.log(state);
+                if(state != 'placed'){
+                    if(state==='cancelled'){
+                        alert("The order is already cancelled!");
+                        return;
+                    }
+
+                    alert("You can't cancel orders that are already shipped or delivered!");
+                    return;
+                }
+                if(!confirm("Are you sure you want to cancel this order?")) return;
+                const orderId = cancelBtn.parentElement.dataset.orderId;
+                await cancelOrder(orderId);
+                await renderOrdersPage(page);
+            });
+       });
     }
     catch{
         commandBox.innerHTML='<p>Nu am putut incarca comenzile dvs!</p>';
     }
 }
 
-renderCommandsPage(currentPage);
+renderOrdersPage(currentPage);
 
 document.getElementById("btn__prev").addEventListener('click', () => {
-    renderCommandsPage(currentPage-1);
+    renderOrdersPage(currentPage-1);
 });
 
 document.getElementById("btn__next").addEventListener('click', () =>{
-    renderCommandsPage(currentPage+1);
+    renderOrdersPage(currentPage+1);
 });
