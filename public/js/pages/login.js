9c5ed442a5cec70a2f7c1a73dce18bfd125de277
gabiturbinca <gabiturbinca2@gmail.com>
2026-05-26 17:16:41 +0300

    bug fix

--- public/js/pages/login.js
+++ public/js/pages/login.js
@@ -25,9 +25,10 @@ form.addEventListener('submit', async (e) => {
     else{
       message_user.textContent="";
       const flagHasToChangePassword=json.success.must_change_password;
-      if(flagHasToChangePassword=="true")
+      if(flagHasToChangePassword===true)
       {
           window.location.href = '/changePassword.php';
+          return;
       }
       message_user.classList.remove("error__message");
       window.location.href = '/home.php';
